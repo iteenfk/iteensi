0b151f7318c911e1c8edf3e4b134b34fe408d7f8
iteenfk <125781734+iteenfk@users.noreply.github.com>
2023-04-28 20:13:55 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,36 +1,45 @@
 "use strict"
 
-function displayCalendar() {
-    const monthSelect = document.getElementById("monthSelect");
-    const monthIndex = monthSelect.selectedIndex;
-    const monthValue = monthSelect.options[monthIndex].value;
+const api_key = "50d4c65fa058ccffec4d18694c037a13";
+
+const select = document.getElementById("select");
 
 
+select.addEventListener("change", () => {
+    const city = select.value;
+    console.log(city);
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`;
 
-    const date = new Date();
-    date.setMonth(monthValue);
-    date.setDate(1);
+    console.log(url)
+    
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const a = data.weather[0].main;
+        const b = data.main.temp;
+        const c = data.main.humidity;
+        const d = data.wind.speed;
+        const e = data.wind.deg;
+        const f = data.main.pressure;
+        const g = data.main.temp_max;
+        const h = data.main.temp_min;
+        const i = new Date(data.sys.sunrise * 1000).toLocaleTimeString();
+        const j = new Date(data.sys.sunset * 1000).toLocaleTimeString();
 
-
-    let calendarHTML = "<table>";
-    calendarHTML +="<tr><th>日</th><th>月</th><th>火</th><th>水</th><th>木</th><th>金</th><th>土</th></tr>"
-
-    while (date.getMonth() == monthValue) {
-        calendarHTML +="<tr>";
-        for (let i =0; i < 7;i++){
-            if (date.getDay() == i){
-                calendarHTML += "<td>" + date.getDate() + "</td>";
-                date.setDate(date.getDate() + 1);
-            } else {
-                calendarHTML += "<td></td>"
-            }
-        }
-        calendarHTML += "</tr>";
-    }
-
-    calendarHTML += "</table>";
-
-    console.log(calendarHTML);
-
-    document.getElementById("calendar").innerHTML = calendarHTML;
-}
+        const Box = document.getElementById("box");
+        Box.innerHTML = `
+        <h3>${city}の天気予報</h3>
+        <p>天気: ${a}</p>
+        <p>気温: ${b}</p>
+        <p>湿度: ${c}</p>
+        <p>風速: ${d}</p>
+        <p>風向: ${e}</p>
+        <p>気圧: ${f}</p>
+        <p>最高気温: ${g}</p>
+        <p>最低気温: ${h}</p>
+        <p>日の出時刻: ${i}</p>
+        <p>日没時刻: ${j}</p>
+        `
+    })
+    .catch(error => console.error(error));
+});
