b7238eff74a5641c54187a4b0ac765f4fbff5d08
iteenfk <125781734+iteenfk@users.noreply.github.com>
2023-06-16 20:24:42 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,22 +1,40 @@
 "use strict"
 
-function S() {
-    let li = document.createElement("li");
-    let inp = document.getElementById("inp").value;
-    li.textContent = inp;
+let words = [];
+function A(){
+    const in1 = document.getElementById("in1")
+    const in2 = document.getElementById("in2")
+    const trim1 = in1.value.trim();
+    const trim2 = in2.value.trim();
 
-    let out = document.getElementById("out");
-    out.appendChild(li);
+    if (!trim1 || !trim2){
+        alert("単語の意味を入力してください");
+        return;
+    }
+    words.push({trim1,trim2});
+    D();
+    in1.value = "";
+    in2.value = "";
+}
 
-    document.getElementById("inp").value = "";
+function D() {
+    const out = document.getElementById("out");
+    out.innerHTML = "";
+    words.forEach(x =>{
+        const li = document.createElement("li");
+        li.textContent = `${x.trim1}: ${x.trim2}`;
+        out.appendChild(li);
+    });
 }
 
 function C(){
     const ul = document.getElementById("out");
-    while (ul.firstChild != null) {
+    while(ul.firstChild != null) {
         ul.removeChild(ul.firstChild);
     }
+    words = [];
 }
+
 
 function P(){
     window.print()
